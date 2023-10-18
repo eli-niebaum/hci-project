57067e2a5a84b692b13e170cdd9d6af1c559a280
dynamicdonut44 <eliniebaum@unomaha.edu>
2023-10-17 19:35:20 -0500

    basic lo-fi example

--- project.py
+++ project.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+print("!!!WELCOME!!!")
+print("-------------")
+
+print("Enter Creepy Room?")
+var = input("Yes or No?")
+
+if var == "Yes":
+    print("Use the Calculator to find the code to open the door to the room.")
+elif var == "No":
+    print("GAME OVER - You didn't even start :(")
+else:
+    print("WRONG INPUT")
