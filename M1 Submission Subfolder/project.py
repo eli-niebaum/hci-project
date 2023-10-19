8642597e138071d2eefdbff4b44aa553a3529e20
dandydragon <89354011+dandydragon@users.noreply.github.com>
2023-10-18 22:16:20 -0500

    M1 Submission Subfolder

---
+++ M1 Submission Subfolder/project.py
@@ -0,0 +1,12 @@
+print("!!!WELCOME!!!")
+print("-------------")
+
+print("Enter Creepy Room?")
+var = input("Yes or No?\n")
+
+if var == "Yes":
+    print("Use the Calculator to find the code to open the door to the room.")
+elif var == "No":
+    print("GAME OVER - You didn't even start :(")
+else:
+    print("WRONG INPUT")
